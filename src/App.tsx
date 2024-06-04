6f9eb1ee50645382f98941a43159fa61e3930bda
emerss001 <emersonnevess80@gmail.com>
2024-06-04 15:36:08 -0300

    feat: adding dialog for creating users

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,22 @@ import {
   TableHeader,
   TableRow,
 } from "./components/ui/table";
+import { Dialog } from "@radix-ui/react-dialog";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
+import { Label } from "./components/ui/label";
 
 export function App() {
   return (
     <div className="p-6 max-w-4xl mx-auto space-y-4">
+      <h1 className="text-4xl font-bold">Users 👨‍💻</h1>
       <div className="flex items-center justify-between">
         <form className="flex items-center gap-2">
           <Input name="username" placeholder="Username" />
@@ -26,18 +38,57 @@ export function App() {
             Filter Users
           </Button>
         </form>
-        <Button>
-          <PlusCircle size={18} className="mr-2" />
-          New User
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <PlusCircle size={18} className="mr-2" />
+              New User
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>New User</DialogTitle>
+              <DialogDescription>
+                Create new user in the system
+              </DialogDescription>
+            </DialogHeader>
+
+            <form className="space-y-6">
+              <div className="grid grid-cols-4 items-center text-right gap-10">
+                <Label htmlFor="username">Username</Label>
+                <Input type="text" className="col-span-3" id="username"></Input>
+              </div>
+
+              <div className="grid grid-cols-4 items-center text-right gap-10">
+                <Label htmlFor="email">Email</Label>
+                <Input type="email" className="col-span-3" id="email"></Input>
+              </div>
+
+              <div className="grid grid-cols-4 items-center text-right gap-10">
+                <Label htmlFor="age">Age</Label>
+                <Input type="number" className="col-span-3" id="age"></Input>
+              </div>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button type="button" variant={"outline"}>
+                    Cancelar
+                  </Button>
+                </DialogClose>
+                <Button type="submit">Salvar</Button>
+              </DialogFooter>
+            </form>
+          </DialogContent>
+        </Dialog>
       </div>
-      <div className="border rounded-lg p-2">
+      <div className="border rounded-lg p-2 font-mono">
         <Table>
           <TableHeader>
-            <TableHead>ID</TableHead>
-            <TableHead>NOME</TableHead>
-            <TableHead>EMAIL</TableHead>
-            <TableHead>IDADE</TableHead>
+            <TableRow>
+              <TableHead>ID</TableHead>
+              <TableHead>NOME</TableHead>
+              <TableHead>EMAIL</TableHead>
+              <TableHead>IDADE</TableHead>
+            </TableRow>
           </TableHeader>
           <TableBody>
             {Array.from({ length: 10 }).map((_, i) => {
