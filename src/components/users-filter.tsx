bd40db8c81ee3e004d79d5e814cd446e88a5fab3
emerss001 <emersonnevess80@gmail.com>
2024-06-05 11:19:32 -0300

    feat: typing and validating filter form data

--- src/components/users-filter.tsx
+++ src/components/users-filter.tsx
@@ -1,12 +1,32 @@
 import { Search } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const usersFilterSchema = z.object({
+  username: z.string(),
+  age: z.coerce.number(),
+});
+
+type UsersFilterSchema = z.infer<typeof usersFilterSchema>;
 
 export function UsersFilter() {
+  const { register, handleSubmit } = useForm<UsersFilterSchema>({
+    resolver: zodResolver(usersFilterSchema),
+  });
+
+  function handleFilterUsers(data: UsersFilterSchema) {
+    console.log(data);
+  }
   return (
-    <form className="flex items-center gap-2">
-      <Input name="username" placeholder="Username" />
-      <Input name="age" placeholder="User age" />
+    <form
+      onSubmit={handleSubmit(handleFilterUsers)}
+      className="flex items-center gap-2"
+    >
+      <Input placeholder="Username" {...register("username")} />
+      <Input placeholder="User age" {...register("age")} />
       <Button
         type="submit"
         variant={"secondary"}
